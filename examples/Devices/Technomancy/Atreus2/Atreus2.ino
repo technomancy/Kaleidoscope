bdc28f81a54df70ccc8c9d40b438b351e7045a59
Phil Hagelberg <phil@hagelb.org>
2019-08-26 15:04:54 -0700

    Switching up the fn layer pretty drastically.

--- examples/Devices/Technomancy/Atreus2/Atreus2.ino
+++ examples/Devices/Technomancy/Atreus2/Atreus2.ino
@@ -41,33 +41,33 @@ KEYMAPS(
   (
        Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
       ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B ,XXX
+      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B         ,Key_Backtick
       ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
 
-                    ,Key_Y     ,Key_U ,Key_I     ,Key_O      ,Key_P
-                    ,Key_H     ,Key_J ,Key_K     ,Key_L      ,Key_Semicolon
-       ,XXX         ,Key_N     ,Key_M ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt ,Key_Space ,MO(1) ,Key_Minus ,Key_Quote  ,Key_Enter
+                      ,Key_Y     ,Key_U ,Key_I     ,Key_O      ,Key_P
+                      ,Key_H     ,Key_J ,Key_K     ,Key_L      ,Key_Semicolon
+       ,Key_Backslash ,Key_N     ,Key_M ,Key_Comma ,Key_Period ,Key_Slash
+       ,Key_LeftAlt   ,Key_Space ,MO(1) ,Key_Minus ,Key_Quote  ,Key_Enter
   ),
 
   [1] = KEYMAP_STACKED
   (
-       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_LeftCurlyBracket ,Key_RightCurlyBracket
-      ,Key_Hash        ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_Dollar
-      ,Key_LeftBracket ,Key_RightBracket ,Key_LeftParen ,Key_RightParen       ,Key_And               ,XXX
+       Key_Exclamation ,Key_At           ,Key_Hash      ,Key_Dollar           ,Key_Percent
+      ,Key_LeftParen   ,Key_LeftArrow    ,Key_UpArrow   ,Key_RightArrow       ,Key_RightParen
+      ,Key_LeftBracket ,Key_RightBracket ,Key_DownArrow ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
       ,TG(2)           ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Backspace         ,Key_LeftControl
 
-                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Star
-                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,Key_Plus
-      ,XXX         ,Key_Backtick ,Key_1 ,Key_2      ,Key_3 ,Key_Backslash
+                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
+                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,XXX
+      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
       ,Key_LeftAlt ,Key_Space    ,MO(1) ,Key_Period ,Key_0 ,Key_Equals
    ),
 
   [2] = KEYMAP_STACKED
   (
-       Key_Insert ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
-      ,Key_Delete ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
-      ,XXX        ,Consumer_VolumeIncrement ,XXX           ,XXX            ,M(RESET)     ,XXX
+       Key_Insert ,Key_Home                 ,XXX           ,Key_End        ,Key_PageUp
+      ,Key_Delete ,Key_LeftArrow            ,Key_UpArrow   ,Key_RightArrow ,Key_PageDown
+      ,XXX        ,Consumer_VolumeIncrement ,Key_DownArrow ,XXX            ,M(RESET)     ,XXX
       ,XXX        ,Consumer_VolumeDecrement ,___           ,___            ,___          ,___
 
                 ,Key_UpArrow   ,Key_F7 ,Key_F8          ,Key_F9         ,Key_F10
