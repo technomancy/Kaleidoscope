28ce181cc5dad68b0d39a5b00a5a36716642f6c3
Phil Hagelberg <phil@hagelb.org>
2020-02-24 17:10:52 -0800

    Add multidvorak layout.

--- examples/Devices/Technomancy/Atreus2/Atreus2.ino
+++ examples/Devices/Technomancy/Atreus2/Atreus2.ino
@@ -33,7 +33,7 @@
 #define TG(n) LockLayer(n)
 
 enum {
-  LAYER_QWERTY
+  LAYER_PRIMARY
 };
 
 #define Key_Exclamation LSHIFT(Key_1)
@@ -45,16 +45,28 @@ enum {
 #define Key_And LSHIFT(Key_7)
 #define Key_Star LSHIFT(Key_8)
 #define Key_Plus LSHIFT(Key_Equals)
+#define Key_Underscore LSHIFT(Key_Minus)
+
+// change this to select an alternate layout.
+// #define PRIMARY_KEYMAP_QWERTY
+#define PRIMARY_KEYMAP_DVORAK
 
 enum {
-  QWERTY,
+  PRIMARY,
   FUN,
-  UPPER
+  UPPER,
+  HARD,
+  HARDFUN
 };
+
+// note: the backslash on the fun layer is redundant and kept more for
+// muscle-memory compatibility with the Atreus 1, but if we find a better
+// use for it we can get rid of it.
 
 /* *INDENT-OFF* */
 KEYMAPS(
-  [QWERTY] = KEYMAP_STACKED
+#if defined (PRIMARY_KEYMAP_QWERTY)
+  [PRIMARY] = KEYMAP_STACKED
   (
        Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
       ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
@@ -66,7 +78,6 @@ KEYMAPS(
        ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
        ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
   ),
-
   [FUN] = KEYMAP_STACKED
   (
        Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
@@ -75,22 +86,52 @@ KEYMAPS(
       ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Backspace         ,Key_LeftControl
 
                    ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
-                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
+                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,Key_Backslash
       ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
       ,Key_LeftAlt ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
    ),
+#elif defined (PRIMARY_KEYMAP_DVORAK)
+  [PRIMARY] = KEYMAP_STACKED
+  (
+       Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
+      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
+      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
+      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+
+                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
+                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
+       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
+       ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Quote ,Key_LeftBracket  ,Key_Enter
+  ),
+  [FUN] = KEYMAP_STACKED
+  (
+       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
+      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
+      ,Key_Minus ,Key_Equals ,Key_Hash      ,Key_Underscore ,Key_Plus ,Key_Caret
+      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Backspace         ,Key_LeftControl
+
+                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
+                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,Key_Backslash
+      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_RightCurlyBracket
+      ,Key_LeftAlt ,Key_Space    ,___   ,Key_E      ,Key_0 ,Key_RightBracket
+   ),
+#else
+
+#error "No default keymap defined. You should make sure that you have a line like '#define PRIMARY_KEYMAP_QWERTY' in your sketch"
+
+#endif
 
   [UPPER] = KEYMAP_STACKED
   (
        Key_Insert ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
       ,Key_Delete ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
       ,XXX        ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
-      ,M(QWERTY)  ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+      ,M(PRIMARY)  ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
 
                 ,Key_UpArrow   ,Key_F7 ,Key_F8          ,Key_F9         ,Key_F10
                 ,Key_DownArrow ,Key_F4 ,Key_F5          ,Key_F6         ,Key_F11
       ,___      ,XXX           ,Key_F1 ,Key_F2          ,Key_F3         ,Key_F12
-      ,___      ,___           ,M(QWERTY)  ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
+      ,___      ,___           ,M(PRIMARY)  ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
    )
 )
 /* *INDENT-ON* */
@@ -110,8 +151,8 @@ KALEIDOSCOPE_INIT_PLUGINS(
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
-  case QWERTY:
-    Layer.move(LAYER_QWERTY);
+  case PRIMARY:
+    Layer.move(LAYER_PRIMARY);
     break;
   default:
     break;
